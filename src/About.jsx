bb85746cb63d3d4373b4e8f2dace45e4d3da5657
Godfred-A99 <godfredadjetey99@gmail.com>
2024-08-25 11:23:45 +0000

    finishing the about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,31 @@
-
-
 const About = () => {
   return (
     <section id="About">
-        <div>
-            <div>
-                
-            </div>
+      <div className="flex justify-between items-center gap-5">
+        <div className="w-3/5">
+          <div></div>
+          <img
+            src="/src/Assets/Images/pg1.png"
+            alt="picture2"
+            width={300}
+            height={172}
+          />
         </div>
+        <div className="w-2/5 text-center text-white ">
+          <h1 className="font-Roboto_Slab text-2xl">About</h1>
+          <hr />
+          <p className=" font-Mulish text-sm">
+            Godfred Adjetey is a dedicated web designer and developer with two
+            years of experience in the field. Currently, he is a student at
+            Accra Technical University, where he continues to refine his skills
+            and deepen his knowledge in web development. His passion for
+            creating dynamic and user-friendly websites drives his commitment to
+            continuous learning and professional growth.
+          </p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./About";
 import Hero from "./Hero";
 import NavBar from "./NavBar";
 
@@ -7,8 +8,11 @@ function App() {
       <section className="w-full px-24 pt-5 max-sm:px-5 bg-bg-color">
         <NavBar />
       </section>
-      <section className="px-24 max-sm:px-5 bg-bg-color">
+      <section className="px-24 w-full max-sm:px-5 bg-bg-color">
         <Hero />
+      </section>
+      <section className="px-24 w-full bg-bg-color ">
+        <About />
       </section>
     </>
   );
