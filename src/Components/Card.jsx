7139ed3420207270507902141c516f0f6364ec4b
Godfred-A99 <godfredadjetey99@gmail.com>
2024-08-28 02:31:03 +0000

    fixing the card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,20 @@
-import Button from "./Button"
+import Button from "./Button";
 
-
-const Card = ({img,alt,title,description}) => {
+const Card = ({ img, alt, title, description, width, height }) => {
   return (
     <div className="w-80 bg-bg-color rounded-lg">
-        <img src={img} alt={alt} />
-        <div className="flex justify-center items-center">
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <Button />
-        </div>
+      <a href="">
+        <img src={img} alt={alt} width={width} height={height} />
+      </a>
+      <div className="flex justify-center items-center">
+        <h3 className="font-Roboto_Slab text-white  text-lg text-center font-semibold">
+          {title}
+        </h3>
+        <p className="font-Mulish text-gray-300 text-sm">{description}</p>
+        <Button />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
