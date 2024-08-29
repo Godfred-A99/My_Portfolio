a171ecba28e372147589f330f3d8d1496da866f1
Godfred-A99 <godfredadjetey99@gmail.com>
2024-08-29 02:43:55 +0000

    done creating the blog section

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,15 +1,19 @@
-
+import Card from "./Components/Card";
 
 const Blog = () => {
   return (
-    <section className="">
-        <div>
-            <h1>Blogs</h1>
-            <div></div>
+    <section className="bg-bg-color w-full flex  xl:flex-row flex-col justify-between items-center min-h-screen gap-5 max-sm:px-5">
+      <div className="">
+        <h1 className="font-Roboto_Slab text-center text-4xl text-white p-1">Blogs</h1>
+        <hr className="m-auto border-bg-div"/>
+        <div className="flex justify-center items-center gap-4 py-10">
+           <Card />
+           <Card />
+           <Card />
         </div>
-
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
