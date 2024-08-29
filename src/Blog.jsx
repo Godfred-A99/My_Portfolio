31cbbbf23a82730572f20edbae26afceed98c837
Godfred-A99 <godfredadjetey99@gmail.com>
2024-08-29 02:31:16 +0000

    adding blog section

---
+++ src/Blog.jsx
@@ -0,0 +1,15 @@
+
+
+const Blog = () => {
+  return (
+    <section className="">
+        <div>
+            <h1>Blogs</h1>
+            <div></div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Blog
